1b534b123224169a6704a58d562ac8a6526df93c
Chris Hansen <moosekid5@gmail.com>
2012-06-05 15:19:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuxedoweasels::Application.config.secret_token = '58d693b48e08f0b4691b3b563d7c67768bca3da709520c18ffa086fa17bf6a568dcc92e904c0e8b00e7165baebb56a332bbf66f96c401787b1f581c19c978336'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tuxedoweasels::Application.config.session_store :cookie_store, key: '_tuxedoweasels_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tuxedoweasels::Application.config.session_store :active_record_store
